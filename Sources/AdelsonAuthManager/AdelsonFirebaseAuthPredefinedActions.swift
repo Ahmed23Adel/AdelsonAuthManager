95410dfe288da1a8edc491f1e38542d2681cb6dc
Ahmed23Adel <ahmed.hussein.1521@gmail.com>
2026-01-31 22:02:06 +0100

    adding support for firebase applications

---
+++ Sources/AdelsonAuthManager/AdelsonFirebaseAuthPredefinedActions.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  AdelsonAuthManager
+//
+//  Created by ahmed on 31/01/2026.
+//
+
+import Foundation
---
+++ Sources/AdelsonAuthManager/Config/AdelsonFirebaseAuthConfig.swift
@@ -0,0 +1,36 @@
+//
+//  File.swift
+//  AdelsonAuthManager
+//
+//  Created by ahmed on 31/01/2026.
+//
+
+import Foundation
+
+/// No Storing to token, as it's expected to get it right from firebase
+/// something like
+/// import FirebaseAuth
+
+/// func getIDToken() async throws -> String {
+///    guard let user = Auth.auth().currentUser else {
+///        throw NSError(domain: "AuthError", code: 0, userInfo: [NSLocalizedDescriptionKey: "No user logged in"])
+///    }
+///    let idToken = try await user.getIDToken(forcibly: false)
+///    return idToken
+///}
+@MainActor
+public final class AdelsonFirebaseAuthConfig: Sendable{
+    let appName: String
+    let baseUrl: String
+    let fnFirebaseIdToken: () -> String
+
+    public init(appName: String,
+         baseUrl: String,
+         fnFirebaseIdToken: @escaping () -> String
+    ){
+        self.appName = appName
+        self.baseUrl = baseUrl
+        self.fnFirebaseIdToken = fnFirebaseIdToken
+    }
+       
+}
