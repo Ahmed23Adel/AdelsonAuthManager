a975d63dede98d2817e31726597ddefa2be352cb
Ahmed23Adel <ahmed.hussein.1521@gmail.com>
2026-01-31 22:04:48 +0100

    adding predefined actoins for firebase support

--- Sources/AdelsonAuthManager/AdelsonFirebaseAuthPredefinedActions.swift
+++ Sources/AdelsonAuthManager/AdelsonFirebaseAuthPredefinedActions.swift
@@ -6,3 +6,21 @@
 //
 
 import Foundation
+
+
+@available(macOS 10.15, *)
+public actor AdelsonFirebaseAuthPredefinedActions{
+    public static let shared = AdelsonFirebaseAuthPredefinedActions()
+    
+    public func wakeUp(appName: String,
+                       baseUrl: String,
+                       fnFirebaseIdToken: @Sendable @escaping () -> String
+    ) async -> AdelsonFirebaseAuthConfig {
+        let config = await AdelsonFirebaseAuthConfig(
+            appName: appName,
+            baseUrl: baseUrl,
+            fnFirebaseIdToken: fnFirebaseIdToken,
+        )
+        return config
+    }
+}
